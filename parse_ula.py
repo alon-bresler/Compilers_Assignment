1a4516347ece34a55c9c8dbe1e9c571e3951c483
Alon Bresler <abresler22@gmail.com>
2015-08-27 13:13:19 +0200

    Created an abstract syntax tree, and commented out the old code in the parse_ula.

--- parse_ula.py
+++ parse_ula.py
@@ -5,37 +5,59 @@ import ply.yacc as yacc
 # Get the token map from the lexer (lex_ula). #
 from lex_ula import tokens
 
-def p_expression_plus(p):
-    'expression : expression PLUS term'
-    p[0] = p[1] + p[3]
+# Setting precedence and associativity #
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE'),
+)
 
-def p_expression_minus(p):
-    'expression : expression MINUS term'
-    p[0] = p[1] - p[3]
+# Binary operations for @, $, #, &
+# def p_binary_operators(p):
+#     '''expression : expression PLUS term
+#                   | expression MINUS term
+#        term       : term TIMES factor
+#                   | term DIVIDE factor'''
+#     if p[2] == '@':
+#         p[0] = p[1] + p[3]
+#     elif p[2] == '$':
+#         p[0] = p[1] - p[3]
+#     elif p[2] == '#':
+#         p[0] = p[1] * p[3]
+#     elif p[2] == '&':
+#         p[0] = p[1] / p[3]
+#
+# def p_expression_term(p):
+#     'expression : term'
+#     p[0] = p[1]
+#
+# def p_term_factor(p):
+#     'term : factor'
+#     p[0] = p[1]
+#
+# def p_factor_num(p):
+#     'factor : NUMBER'
+#     p[0] = p[1]
+#
+# def p_factor_expr(p):
+#     'factor : LPAREN expression RPAREN'
+#     p[0] = p[2]
 
-def p_expression_term(p):
-    'expression : term'
-    p[0] = p[1]
+# Constructing a abstract syntax tree #
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression'''
 
-def p_term_times(p):
-    'term : term TIMES factor'
-    p[0] = p[1] * p[3]
+    p[0] = ('binary-expression',p[2],p[1],p[3])
 
-def p_term_div(p):
-    'term : term DIVIDE factor'
-    p[0] = p[1] / p[3]
+def p_expression_group(p):
+    'expression : LPAREN expression RPAREN'
+    p[0] = ('group-expression',p[2])
 
-def p_term_factor(p):
-    'term : factor'
-    p[0] = p[1]
-
-def p_factor_num(p):
-    'factor : NUMBER'
-    p[0] = p[1]
-
-def p_factor_expr(p):
-    'factor : LPAREN expression RPAREN'
-    p[0] = p[2]
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = ('number-expression',p[1])
 
 # Error rule for syntax errors
 def p_error(p):
