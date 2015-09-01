ee1e06db121c64ab910e853374e63dd7bc34d82a
Alon Bresler <abresler22@gmail.com>
2015-09-01 21:49:29 +0200

    Still working on printing out the ast tree. I've got upto one assignment statement so far.

--- parse_ula.py
+++ parse_ula.py
@@ -5,11 +5,13 @@ __author__ = 'Alon Bresler'
 
 import ply.yacc as yacc
 import lex_ula
+import sys
 
 # Get the token map from the lexer (lex_ula). #
 from lex_ula import tokens
 
 programName = ''
+finalString = "Start\n\tProgram\n\t\t"
 
 # Setting precedence and associativity #
 precedence = (
@@ -63,13 +65,40 @@ parser = yacc.yacc()
 #         print(result[0])
 #         print('\t' + 'ID,' + result[1])
 
+def writeToFile():
+    words = programName.split('.')
+    f = open(words[0] + ".ast", 'w')
+    f.write(finalString)
+    f.close()
+
+def recursiveFormat(result):
+    if (len(result) == 2):
+        global finalString
+        finalString += "\n\t\t\t\t" + result[0] + "\n\t\t\t\t\tFLOAT_LITERAL," + result[1]
+    else:
+        finalString += "\n\t\t\t" + result[0]
+        for i in range (1, len(result)):
+            recursiveFormat(result[i])
+
+
+def formatResult(result):
+    #print("Start")
+    #print("\tProgram")
+
+    #print("\t\t" + result[0])
+    #print("\t\t\tID," + result[1])
+    global finalString
+    finalString += result[0] + "\n\t\t\tID," + result[1]
+
+    recursiveFormat(result[2])
+    print(finalString)
+
 
 
 def processLine(line):
     if (line != ''):
-        print(line)
         result = parser.parse(line)
-        print(result)
+        formatResult(result)
 
 
 def readFromFile(file):
@@ -101,7 +130,7 @@ def readFromFile(file):
            lineForProcessing = dataArr[i]
         else:
             lineForProcessing = lineForProcessing + dataArr[i]
-   processLine(lineForProcessing + max(dataArr))
+   processLine(lineForProcessing + dataArr[len(dataArr)-1])
 
 
 
@@ -109,7 +138,14 @@ def readFromFile(file):
 
 #RUN MAIN
 if __name__ == '__main__':
+    programName = 'myExamples/basicAssignment.ula'
+    lex_ula.programName = 'myExamples/basicAssignment.ula'
+    lex_ula.readFromFile('myExamples/basicAssignment.ula')
+    readFromFile('myExamples/basicAssignment.ula')
+
     #programName = sys.argv[1]
-    lex_ula.programName = 'ula_samples/numbers.ula'
-    lex_ula.readFromFile('ula_samples/numbers.ula')
-    readFromFile('ula_samples/numbers.ula')
+    #lex_ula.programName = sys.argv[1]
+    #lex_ula.readFromFile(sys.argv[1])
+    #readFromFile(sys.argv[1])
+
+    writeToFile()
