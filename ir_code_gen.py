886a4b74e9d311bed1fa38815f0cc7cccb875cfc
Alon Bresler <abresler22@gmail.com>
2015-09-17 22:14:42 +0200

    Commented code in the ir_code_gen - to help with understanding.

--- ir_code_gen.py
+++ ir_code_gen.py
@@ -13,20 +13,21 @@ def code_gen(tree): # traverse tree recursively to generate code
             code_gen(t)
     elif tree[0] == "=":
         last_var = tree[1][0]
-        var_dict[last_var] = builder.alloca(ir.FloatType())
-        builder.store(code_gen(tree[2]), var_dict[last_var])
+        var_dict[last_var] = builder.alloca(ir.FloatType())                             ## builder.alloca --> statically allocate stack slot for size va;ues of type typ.
+        builder.store(code_gen(tree[2]), var_dict[last_var])                            ## buidler.store --> Store value to pointer ptr
     elif tree[0] == "+":
-        return(builder.fadd(code_gen(tree[1]),code_gen(tree[2])))
+        return(builder.fadd(code_gen(tree[1]),code_gen(tree[2])))                       ## builder.fadd --> floating-point added to LHS and RHS
     elif tree[0].isnumeric():
         return(ir.Constant(ir.FloatType(), float(tree[0])))
 
 
 flttyp = ir.FloatType() # create float type
 fnctyp = ir.FunctionType(flttyp, ()) # create function type to return a float
-module = ir.Module(name="ula") # create module named "ula"
+module = ir.Module(name="ula") # create module named "ula"                              ## compilation unit --> defines set of related functions, global vairables and metadata.
 func = ir.Function(module, fnctyp, name="main") # create "main" function
-block = func.append_basic_block(name="entry") # create block "entry" label
-builder = ir.IRBuilder(block) # create irbuilder to generate code
+block = func.append_basic_block(name="entry") # create block "entry" label              ## the basic block the builder is oeprating on ##
+builder = ir.IRBuilder(block) # create irbuilder to generate code                       ## fill the basic blocks with LLVM instructions || has pointer to block's list of instructions
+                                                                                        ## starts at the end of basic block
 code_gen(tree) # call code_gen() to traverse tree & generate code
 builder.ret(builder.load(var_dict[last_var])) # specify return value
 print(module)
