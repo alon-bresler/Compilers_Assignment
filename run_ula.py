04d6cb92c34adab57c7246f661dadbd8e61142af
Alon Bresler <abresler22@gmail.com>
2015-09-18 19:59:50 +0200

    About to submit to automarker --> cfunc changed parameters.

--- run_ula.py
+++ run_ula.py
@@ -1,7 +1,5 @@
-__author__ = 'Alon'
-
 from __future__ import print_function
-from ctypes import CFUNCTYPE, c_double
+from ctypes import CFUNCTYPE, c_double, c_float
 import llvmlite.binding as llvm
 
 import ir_ula
@@ -13,7 +11,18 @@ llvm.initialize()
 llvm.initialize_native_target()
 llvm.initialize_native_asmprinter()  # yes, even this one
 
-llvm_ir = ''
+llvm_ir = """
+   ; ModuleID = "examples/ir_fpadd.py"
+   target triple = "unknown-unknown-unknown"
+   target datalayout = ""
+
+   define double @"fpadd"(double %".1", double %".2")
+   {
+   entry:
+     %"res" = fadd double %".1", %".2"
+     ret double %"res"
+   }
+   """
 
 def create_execution_engine():
     """
@@ -43,21 +52,38 @@ def compile_ir(engine, llvm_ir):
     engine.finalize_object()
     return mod
 
-def readInIRFile():
-    print("")
+def readInIRFile(file):
+    words = file.split('.')
+    f = open(words[0] + ".ir", 'r')
+    global llvm_ir
+    llvm_ir = ""
+    for line in f:
+        llvm_ir = llvm_ir + line
+
+def writeToFile(m):
+    #open the file
+    words = fileName.split('.')
+    f = open(words[0] + ".run", 'w')
+    f.write(str(m))
+    f.close()
+
 
 #RUN MAIN
 if __name__ == '__main__':
 
-
+    global fileName
+    #fileName = sys.argv[1]
+    fileName = 'ula_irrun_samples/add.ula'
+    ir_ula.createIntermediateRepresentation(fileName)
+    readInIRFile(fileName)
 
     engine = create_execution_engine()
     mod = compile_ir(engine, llvm_ir)
 
     # Look up the function pointer (a Python int)
-    func_ptr = engine.get_function_address("fpadd")
+    func_ptr = engine.get_function_address("main")
 
     # Run the function via ctypes
-    cfunc = CFUNCTYPE(c_double, c_double, c_double)(func_ptr)
-    res = cfunc(1.0, 3.5)
-    print("fpadd(...) =", res)
+    cfunc = CFUNCTYPE(c_float)(func_ptr)
+    res = cfunc()
+    writeToFile(res)
